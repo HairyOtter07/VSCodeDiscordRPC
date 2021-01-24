f1f024382683abadaff702f3f46c36fd007c7043
HairyOtter07 <66044018+HairyOtter07@users.noreply.github.com>
2021-01-24 11:39:43 -0800

    clean up extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,10 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as rpc from 'discord-rpc';
-import { time } from 'console';
 
 const clientID = "802630166562996255";
 const client = new rpc.Client({transport: "ipc"});
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscordrpc" is now active!');
 
 	let disposable2 = vscode.commands.registerCommand('vscordrpc.runRPC', () => {
 		vscode.window.showInformationMessage('Running RPC...');
@@ -73,5 +64,3 @@ export function activate(context: vscode.ExtensionContext) {
 	vscode.commands.executeCommand("vscordrpc.runRPC");
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
